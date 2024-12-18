fa82161681c0624776c4fd450ad8494c629207f9
Ayan Upadhaya <ayanu881@gmail.com>
2024-12-18 21:08:33 +0600

    user dashboard created

--- frontend/src/pages/dashboards/user/home/UserHome.jsx
+++ frontend/src/pages/dashboards/user/home/UserHome.jsx
@@ -1,7 +1,87 @@
+import axios from "axios";
 import useAuth from "../../../../hooks/useAuth";
+import { getToken } from "../../../../utils/getToken";
+import { useEffect, useState } from "react";
 
 function UserHome() {
   const { user } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // Base URL for the API
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const [orders, setOrders] = useState([]);
+  const [storeBooks, setStoreBooks] = useState([]);
+
+  const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+      Authorization: `Bearer ${getToken()}`,
+    },
+  });
+
+  // Fetch orders by store
+  const fetchOrdersByStore = async (storeId) => {
+    setLoading(true)
+    setError(null)
+    try {
+
+      const response = await axiosInstance.get(`/orders/store/${storeId}`);
+      setOrders(response.data.data);
+    } catch (err) {
+      console.error(err);
+      setError(err)
+    } finally{
+      setLoading(false)
+    }
+  };
+
+  const getBooksByStore = async (storeId) => {
+    setLoading(true)
+    setError(null)
+    try {
+      const response = await axiosInstance.get(`/books/store/${storeId}`);
+      if (response?.data) {
+        setStoreBooks(response?.data);
+        return response.data;
+      }
+    } catch (err) {
+      console.error(err);
+      setError(err)
+    } finally{
+      setLoading(false)
+    }
+  };
+
+  function callApi() {
+    fetchOrdersByStore(user?.store_id)
+    getBooksByStore(user?.store_id);
+  }
+
+  useEffect(() => {
+    callApi();
+  }, []);
+
+  if (error) {
+    return (
+      <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+        <div>
+          <h2>Something went wrong</h2>
+        </div>
+      </div>
+    );
+  }
+
+  if (loading) {
+    return (
+      <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+        <div>
+          <h2>Loading...</h2>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <div className="w-100 h-100">
@@ -13,8 +93,27 @@ function UserHome() {
             </div>
           </div>
           {/* table */}
-          <div>
-            <h4>Welcome {(user && user?.name) || "user"}</h4>
+          <div className="container">
+            <div class="row">
+              <div class="col-sm-6">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Total Books</h5>
+                    <p class="card-text fw-bold text-lg">
+                      {storeBooks?.length}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Total Orders</h5>
+                    <p class="card-text fw-bold text-lg">{orders?.length}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
